f0a092c5ec97c4bcc7db19adac2ebd5d66421382
Corey Schadeck <corey@schadeck.com>
2022-10-26 11:29:05 -0400

    updated wsgi.py line 14 for deployment

--- thoughts/wsgi.py
+++ thoughts/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thoughts.settings")
+os.environ.setdefaultz["DJANGO_SETTINGS_MODULE"] = "thoughts.settings"
 
 application = get_wsgi_application()
